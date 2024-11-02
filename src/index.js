e2e5764f2d9d299470a812d729f94aa2447ffdd8
Jaron <jaronchretien@yahoo.com>
2024-11-02 12:34:58 -0500

    feat(App): adds app js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 const root = ReactDOM.createRoot(document.getElementById('app'))
-
+import App from "./App"
 root.render(
-    <h1>Hello World</h1>
+    <App />
 )
