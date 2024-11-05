db92d2079571fbfa82b3abcafc455d24ba98c205
Jaron <jaronchretien@yahoo.com>
2024-11-04 21:46:25 -0600

    feat(slide): Adds slide into new color palette

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ const App = () => {
     const [numOfPalletes, setNumOfPalletes] = useState(3)
     const [pallete, setPallete] = useState([])
     
+    useEffect(() => {
+        generatePallete()
+    }, [])
     const generatePallete = () => {
         let pallete = []
         for (let i = 0; i < numOfPalletes; i++) {
-            // Logic to generate hex color, e.g., random or by incrementing values
             const color = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
             pallete.push(color);
         }
@@ -16,9 +18,19 @@ const App = () => {
     }
    
     const addPallete = () => {
-        if(currentNumber == 5){
+        if(numOfPalletes == 5 || pallete.length == 0){
             return "Max number of pallets"
         }
+        let lastPallete = pallete[0]
+        let hex = parseInt(lastPallete.slice(1), 16)
+        hex += 1
+        console.log(hex, hex > 16777215)
+        if (hex > 16777215) {
+            hex = 16777215
+        }
+        let hexCode = `#${hex.toString(16)}`
+        setNumOfPalletes(current => current += 1)
+        setPallete(current => [...current, hexCode])
     }
 
     return (
